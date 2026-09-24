44b3ba744a88a9aa00d1935be08114eb92e37fa8
José Luis Villalobos Jiménez <jl.vj169@gmail.com>
2026-09-24 10:56:29 -0500

    MinSparseTable construction implementation

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from math import log2
 
+
+class MinSparseTable:
+    def __init__(self, arr: list):
+        self.n = len(arr)
+        y = int(log2(self.n)) + 1
+        self.mat = [[0 for _ in range(self.n)] for _ in range(y)]
+        for i in range(0, self.n):
+            self.mat[0][i] = arr[i]
+        for j in range(1, y):
+            rango = self.n - 2**j + 1
+            for i in range(0, rango):
+                off = i + 2 ** (j - 1)
+                self.mat[j][i] = min(self.mat[j - 1][i], self.mat[j - 1][off])
